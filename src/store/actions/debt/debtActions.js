d3a7af5592149c7d96d8d578325eee7e597996cd
TimRadder <raddertim@gmail.com>
2019-11-14 21:51:48 -0500

    Switch To Post
    Changed the Fetch to be a POST instead of a GET as that is what will be used in almost every case that I can currently think of.

--- src/store/actions/debt/debtActions.js
+++ src/store/actions/debt/debtActions.js
@@ -6,17 +6,25 @@ export const createDebt = (debt, dispatch) => {
 export const getDebts = (dispatch) => {
     let debts = {};
     let userID = '2xw2jSh6PonUi4DAZGjQGeYujWcJnjwoPnYjGJIyXbsZ29VcBcrRvwBEKaG7I8cx4jf0FhP7V3ZeiS0tNjD9Qsm2Lo8ANIr8CRD2juxo3a1daPfnPzJVzIzVrYAE2cGZIjApnwsge8UoJh33Aqlt0yoeysugqVEp1994OsId3YdjemVSvAY4UsizSKvSJUiWPiZl03o3fXsw4iCI4ogfdGU0arU7QIaEPo65VkBdxfhO8GnH8dDs8fqG0aPEU8v';
-    fetch('http://debtapi.com/debt/' + userID)
-        .then(res => res.json())
-        .then(res => {
-            let error = void 0;
-            let loading = true;
-            //console.log("Response: ", res);
-            debts = res.data;
-            if(res.code !== 200){
-                error = res.message;
-            }
-            if(debts) {loading = false;}
-            dispatch({type: 'FETCH_DEBTS', debts: debts, error: error, loading: loading});
-        });  
+        
+    fetch('http://debtapi.com/debt', {
+        method: 'POST',
+        header: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({id: userID})
+    })
+    .then(res => res.json())
+    .then(res => {
+        let error = void 0;
+        let loading = true;
+        console.log("Response: ", res);
+        debts = res.data;
+        if(res.code !== 200){
+            error = res.message;
+        }
+        if(debts) {loading = false;}
+        dispatch({type: 'FETCH_DEBTS', debts: debts, error: error, loading: loading});
+    }); 
 }
